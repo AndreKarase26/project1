233c54eac57ce1c45c651caf8aa27ca9fa0c8fa0
Andrey <a.ka144@mail.ru>
2019-05-22 01:00:47 +0300

    Задание 4

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let money = +prompt("Ваш бюджет на месяц?",''),
+
+let money, time;
+
+function start(){
+    money = +prompt("Ваш бюджет на месяц?",'');
     time = prompt("Введите дату в формате YYYY-MM-DD",'');
+
+    while(isNaN(money) || money == "" || money == null){
+        money = +prompt("Ваш бюджет на месяц?",'');
+    }
+}
+start();
 
 let appData = {
     budget: money,
@@ -7,24 +17,44 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++){
-    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
-        b = prompt("Во сколько обойдется?",'');
-    
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-        && a != '' && b!='' && a.length < 50) {
-            appData.expenses[a] = b;
-        } else{
-            alert("Ошибка. Введите корректные данные");
-            i--;
-            continue;
-        }        
-};
+function chooseExpenses(){
+    for (let i = 0; i < 2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+            b = prompt("Во сколько обойдется?",'');
+        
+        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b!='' && a.length < 50) {
+                appData.expenses[a] = b;
+            } else{
+                i--;
+                continue;
+            }        
+    }
+}
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay); 
+
+function checkSavings(){
+    if (appData.savings){
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+            
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
  
+for (let i = 0; i < 3; i++){
+    let a = prompt("Статья необязательных расходов?",'');
+    appData.optionalExpenses.i= a;
+}
